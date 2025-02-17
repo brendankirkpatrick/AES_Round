7f97a09b43b29f0571f0b550e497181bb88f20fe
Brendan Kirkpatrick <kirkpabd@mail.uc.edu>
2025-02-17 13:52:18 -0500

    Create basic CMake project

---
+++ build/src/CMakeFiles/AES.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/brendan/Projects/AES/AES"
+  "/home/brendan/Projects/AES/AES.pdb"
+  "CMakeFiles/AES.dir/main.cpp.o"
+  "CMakeFiles/AES.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
